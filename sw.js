dc2dedd2cb743d421394ee36bc21c312b1f766f7
hsien0529 <hsien0529@gmail.com>
2026-06-01 17:34:13 +0800

    Refresh cached app assets on mobile

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "allowance-pool-v2";
+const CACHE_NAME = "allowance-pool-v3";
 const ASSETS = [
   "./",
   "./index.html",
@@ -23,6 +23,20 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  const freshTypes = ["document", "script", "style"];
+  if (url.origin === self.location.origin && freshTypes.includes(event.request.destination)) {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+    return;
+  }
   event.respondWith(
     caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
